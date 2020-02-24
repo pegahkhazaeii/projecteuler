43fba1fdd360fa29c2b6a123fd100298b0c91ab5
pegahkhazaeii <khazaee_pegah@yahoo.com>
2020-02-24 20:03:47 +0330

    Project Euler,solution of the problem 2

--- Problem2.py
+++ Problem2.py
@@ -3,3 +3,24 @@ Even Fibonacci numbers
 
 """
 
+a = 1
+b = 2
+l=[1,2]
+c = 3
+while c < 4000000:
+    l.append(c)
+    a=b
+    b=c
+    c=a+b
+s=0 
+for i in range(len(l)):
+    
+    if l[i]%2==0:
+        s+=l[i]
+print(s)
+
+     
+    
+    
+
+    
